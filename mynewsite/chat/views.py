d714f23588674c146feeec0ed6fd78734ccf4783
kaydenvu <kayden2k15@gmail.com>
2025-03-23 23:41:36 -0700

    logout button added

--- mynewsite/chat/views.py
+++ mynewsite/chat/views.py
@@ -11,6 +11,5 @@ def index(request):
 
 def room(request, room_name):
     if not request.user.is_authenticated:
-        HttpResponse("<script>alert('You are not logged in!'); window.location.href='/';</script>")
         return redirect('/')  # Redirect to homepage if not authenticated
     return render(request, "chat/room.html", {"room_name": room_name})
--- mynewsite/mynewsite/urls.py
+++ mynewsite/mynewsite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path("", views.Home, name="home"),
@@ -23,4 +24,5 @@ urlpatterns = [
     path("register/", include("accounts.urls")), 
     path("login/", include("login.urls")),
     path("admin/", admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout')
 ]
