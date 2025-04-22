9b83edc99bdc6faa5d21a223ea87d4b786798e34
kaydenvu <kayden2k15@gmail.com>
2025-04-21 23:40:28 -0700

    Added vercel to trusted domains + made environment variables

--- mynewsite/mynewsite/settings.py
+++ mynewsite/mynewsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s8hrwq21@dio@a%lccx7#uobctsc-vq!7jus2xzkmsd)2mrdnl'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-TRUSTED_DOMAINS = ["127.0.0.1", "localhost"]
+TRUSTED_DOMAINS = ["127.0.0.1", "localhost", ".vercel.app"]
 
 # Application definition
 
